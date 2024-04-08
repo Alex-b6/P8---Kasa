a32a508f3d1e194f90ce915b780a4ae83a942a02
AlexB6 <brou.alexandre.pro@gmail.com>
2024-04-08 17:35:46 +0200

    Refact : About & error pages.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,18 +1,32 @@
-import Banner from "../components/HeroBanner";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+import DropdownMenu from "../components/DropdownMenu";
 
-const bannerData = {
-    img: "aboutImage.jpg"
-};
+import HeroBanner from "../components/HeroBanner";
 
 const About = () => {
     return (
-        <div>
-            <Header />
-            <Banner bannerData={bannerData} />
-            ABOUT
-            <Footer />
+        <div className="app">
+           
+            <main>
+                <HeroBanner img="./aboutImage.jpg" />
+                <DropdownMenu title="Fiabilité">
+                    <p>
+                        Les annonces postées sur Kasa garantissent une fiabilité
+                        totale. Les photos sont conformes aux logements, et
+                        toutes les informations sont régulièrement vérifiées par
+                        nos équipes.
+                    </p>
+                </DropdownMenu>
+                <DropdownMenu title="Respect">
+                    <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+                </DropdownMenu>
+                <DropdownMenu title="Service">
+                    <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+                </DropdownMenu>
+                <DropdownMenu title="Sécurité">
+                    <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l&apos;hôte qu&apos;au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
+                </DropdownMenu>
+            </main>
+            
         </div>
     );
 };
